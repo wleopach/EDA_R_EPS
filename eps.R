007b62162da88574445f5c3bc22955488946fd33
wleopach <wleonardop@gmail.com>
2023-03-19 09:29:13 -0400

    update eps.R

--- eps.R
+++ eps.R
@@ -20,9 +20,10 @@ diamonds %>%
   count(color, cut) %>%  
   ggplot(mapping = aes(x = color, y = cut)) +
   geom_tile(mapping = aes(fill = n))
-
+###Reading data from csv
 eps <- read_csv('r_bridge/plots/eps.csv')
 
+###Histogram of variable Cantidad
 ggplot(eps) +
   geom_histogram(mapping = aes(x = Cantidad), binwidth = 2)+
   coord_cartesian(
@@ -30,7 +31,7 @@ ggplot(eps) +
     ylim = c(0,1500)
   )
 
-
+######FREQPOLY CANDITDAD VS DEPARTAMENTO
 p1<-ggplot(data = eps, mapping = aes(x = Cantidad, y = ..density..)) +
   geom_freqpoly(mapping = aes(colour = Departamento), binwidth = 100) +
   coord_cartesian(
@@ -38,7 +39,7 @@ p1<-ggplot(data = eps, mapping = aes(x = Cantidad, y = ..density..)) +
     ylim = c(0,0.01)
   )
 
-
+######FREQPOLY CANDITDAD VS NOMBRE
 p2<-ggplot(data = eps, mapping = aes(x = Cantidad, y = ..density..)) +
   geom_freqpoly(mapping = aes(colour = Nombre), binwidth = 100) +
   coord_cartesian(
@@ -48,7 +49,7 @@ p2<-ggplot(data = eps, mapping = aes(x = Cantidad, y = ..density..)) +
 
 p1/p2
 
-
+#####HEATMAP NOMBRE VS DEPARTAMENTO
 p3 <- eps %>% 
   count(Nombre, Departamento) %>%  
   ggplot(mapping = aes(x = Nombre, y = Departamento)) +
